13f68f7b9a9e992c633029789e73569f85395463
Gilgalad3441 <gil.galad3441@gmail.com>
2022-06-19 17:42:44 +0200

    enlaces de botones y mostrar formularios

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gestion-pppeludos'`, () => {
+  it(`should have as title 'pppeludos-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gestion-pppeludos');
+    expect(app.title).toEqual('pppeludos-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('gestion-pppeludos app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pppeludos-web app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,142 @@
 import { Component } from '@angular/core';
+// Importo el archivo JSON 
+import datos_peludos from 'src/assets/json/datos_peludos.json';
+
+//variables
+var datos: any;
+var isLicencia: string = "";
+var isCastrado: string = "";
+var isLeishmaniasis: string = "";
+var isOperar: string = "";
+var isEnfermedades: string = "";
+var isTratamiento: string = "";
+var isCastrado: string = "";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
-  title = 'gestion-pppeludos';
+  title = 'pppeludos-web';
+  datos: any = datos_peludos;
+  ver_peludo: string = "Ver datos de los peludos";
+  modificar_datos: string = "Modificar datos";
+  aniadir_peludo: string = "Añadir peludo";
+  quitar_peludo: string = "Quitar peludo";
+  document: undefined;
+  formulario = document.getElementById('Formulario');
+  hierarchicalData: any;
+  datosPerros: boolean | undefined = true;
+  form: boolean | undefined = false;
+  aniadir: boolean | undefined;
+  quitar: boolean | undefined;
+
+  licencia(licencia: any) {
+
+    if (licencia.toLowerCase() === "si") {
+      isLicencia = "badge badge-danger  rounded-pill d-inline";
+    } else {
+      isLicencia = "badge badge-success  rounded-pill d-inline";
+    }
+    return isLicencia;
+  }
+
+  castrado(castrado: any) {
+
+    if (castrado.toLowerCase() === "si") {
+      isCastrado = "badge badge-success  rounded-pill d-inline";
+    } else {
+      isCastrado = "badge badge-danger  rounded-pill d-inline";
+    }
+    return isCastrado;
+  }
+
+  leishmaniasis(Leishmaniasis: any) {
+
+    if (Leishmaniasis.toLowerCase() === "si") {
+      isLeishmaniasis = "badge badge-danger  rounded-pill d-inline";
+    } else {
+      isLeishmaniasis = "badge badge-success  rounded-pill d-inline";
+    }
+    return isLeishmaniasis;
+  }
+  pendienteOperar(operar: any) {
+
+    if (operar.toLowerCase() === "si") {
+      isOperar = "badge badge-danger  rounded-pill d-inline";
+    } else {
+      isOperar = "badge badge-success  rounded-pill d-inline";
+    }
+    return isOperar;
+  }
+  enfermedades(enfermedades: any) {
+
+    if (enfermedades.toLowerCase() === "si") {
+      isEnfermedades = "badge badge-danger  rounded-pill d-inline";
+    } else {
+      isEnfermedades = "badge badge-success  rounded-pill d-inline";
+    }
+    return isEnfermedades;
+  }
+
+  tratamiento(tratamiento: any) {
+
+    if (tratamiento.toLowerCase() === "si") {
+      isTratamiento = "badge badge-danger  rounded-pill d-inline";
+    } else {
+      isTratamiento = "badge badge-success  rounded-pill d-inline";
+    }
+    return isTratamiento;
+  }
+
+  mostrarPerros() {
+    this.datosPerros = true
+    this.form = false
+    this.quitar = false
+    this.aniadir = false;
+  }
+
+  modificarPeludo() {
+    this.datosPerros = false
+    this.form = true
+    this.quitar = false
+    this.aniadir = false; 
+  }
+  
+  aniadirPeludo() {
+    this.form = false
+    this.datosPerros = false
+    this.quitar = false
+    this.aniadir = true;
+  }
+  quitarPeludo() {
+    this.form = false
+    this.datosPerros = false
+    this.aniadir = false
+    this.quitar = true;
+  }
+
+  cerrarFormulario() {
+    this.datosPerros = true
+    this.aniadir = false
+    this.quitar = false
+    this.form = false
+  }
+
+  cerrarAniadir() {
+    this.datosPerros = true
+    this.aniadir = false
+    this.quitar = false
+    this.form = false
+  }
+  cerrarQuitario() {
+    this.datosPerros = true
+    this.aniadir = false
+    this.quitar = false
+    this.form = false
+  }
+
 }
+
